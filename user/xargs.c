b3ac53ec8ef44c7d2a8f2b0bb4600e21bbc08042
MC J <jmc8@qq.com>
2023-07-31 22:27:02 +0800

    找半天找不到bug，气死我了，回退

--- user/xargs.c
+++ user/xargs.c
@@ -1,11 +1,11 @@
 /*****************************************************************//**
  * \file   xargs.c
- * \brief  Write a simple version of the UNIX xargs program: its 
- *         arguments describe a command to run, it reads lines from 
- *         the standard input, and it runs the command for each line, 
- *         appending the line to the command's arguments. Your solution 
+ * \brief  Write a simple version of the UNIX xargs program: its
+ *         arguments describe a command to run, it reads lines from
+ *         the standard input, and it runs the command for each line,
+ *         appending the line to the command's arguments. Your solution
  *         should be in the file user/xargs.c
- * 
+ *
  * \author JMC
  * \date   July 2023
  *********************************************************************/
@@ -24,7 +24,7 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2) 
+  if (argc < 2)
   {
     fprintf(STDERR, "usage: xargs <command> ...\n");
     exit(1);
@@ -32,46 +32,48 @@ int main(int argc, char* argv[])
 
   // 读取命令行参数
   char* cmd[MAX_ARG_NUM] = {};
-  for (int i = 1; i < argc; i++) 
+  for (int i = 1; i < argc; i++)
   {
     cmd[i - 1] = argv[i];
   }
 
   // 读取标准输入
-  char buf[MAX_ARG_LEN];
-  int n;
-  if ((n = read(STDIN, buf, MAX_ARG_LEN)) < 0) 
+  char buf, arg[MAX_ARG_LEN];
+  for (int i = 0; read(STDIN, &buf, 1) > 0; )
   {
-    fprintf(STDERR, "xargs: read error\n");
-    exit(1);
-  }
+    if (buf == '\n')
+    {
+      // 读取到换行符，将参数传递给命令
+      arg[i] = '\0';
 
-  // 将标准输入的内容作为命令行参数
-  for (char* beg = buf, *end, arg[MAX_ARG_NUM]; (end = strchr(beg, '\n')); beg = end + 1)
-  {
-    memset(arg, 0, MAX_ARG_NUM);
-    memcpy(arg, beg, end - beg);
-    // arg[end - beg] = '\0';
-    cmd[argc - 1] = arg;
-    int pid = fork();
-    if (pid < 0) 
-    {
-      fprintf(STDERR, "xargs: fork error\n");
-      exit(1);
-    }
-    else if (pid == 0) 
-    {
-      if (exec(argv[1], cmd) < 0) 
+      // fork一个子进程执行命令
+      int pid = fork();
+      if (pid < 0)
       {
-        fprintf(STDERR, "xargs: exec error\n");
+        fprintf(STDERR, "fork error\n");
         exit(1);
       }
+      else if (pid == 0)
+      {
+        // 子进程执行命令
+        cmd[argc - 1] = arg;
+        if (exec(cmd[0], cmd) < 0)
+        {
+          fprintf(STDERR, "exec error\n");
+          exit(1);
+        }
+      }
+      else
+      {
+        // 父进程等待子进程结束
+        wait(NULL);
+        i = 0;
+      }
     }
-    else 
+    else
     {
-      wait(NULL);
+      arg[i++] = buf;
     }
   }
-
   return 0;
 }
