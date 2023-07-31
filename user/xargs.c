18e6b3e8dd0d776622a31166eaf51b0fba8a27dd
MC J <jmc8@qq.com>
2023-07-31 09:59:50 +0800

    完成xargs

--- user/xargs.c
+++ user/xargs.c
@@ -23,52 +23,53 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2) {
+  if (argc < 2) 
+  {
     fprintf(STDERR, "usage: xargs <command> ...\n");
     exit(1);
   }
 
   // 读取命令行参数
-  char* cmd[MAX_ARG_NUM];
-  for (int i = 1; i < argc; i++) {
+  char* cmd[MAX_ARG_NUM] = {};
+  for (int i = 1; i < argc; i++) 
+  {
     cmd[i - 1] = argv[i];
   }
 
   // 读取标准输入
-  char buf[MAX_ARG_LEN];
-  int n;
-  while ((n = read(STDIN, buf, MAX_ARG_LEN)) > 0) {
-    // 读取到的内容以空格分隔
-    int arg_num = 0;
-    char* args[MAX_ARG_NUM];
-    char* p = buf;
-    while (arg_num < MAX_ARG_NUM && p < buf + n) {
-      args[arg_num++] = p;
-      while (p < buf + n && *p != ' ' && *p != '\n') {
-        p++;
-      }
-      *p = '\0';
-      p++;
-    }
-    args[arg_num] = 0;
+  char buf, arg[MAX_ARG_LEN];
+  for (int i = 0; read(STDIN, &buf, 1) > 0; )
+  {
+    if (buf == '\n')
+    {
+      // 读取到换行符，将参数传递给命令
+      arg[i] = '\0';
 
-    // 执行命令
-    int pid = fork();
-    if (pid < 0) {
-      fprintf(STDERR, "fork failed\n");
-      exit(1);
+      // fork一个子进程执行命令
+      int pid = fork();
+      if (pid < 0)
+      {
+        fprintf(STDERR, "fork error\n");
+        exit(1);
+      }
+      else if (pid == 0)
+      {
+        // 子进程执行命令
+        cmd[argc - 2] = arg;
+        exec(cmd[0], cmd);
+        fprintf(STDERR, "exec error\n");
+      }
+      else
+      {
+        // 父进程等待子进程结束
+        wait(0);
+        i = 0;
+      }
     }
-    else if (pid == 0) {
-      // 子进程
-      exec(cmd[0], cmd);
-      fprintf(STDERR, "exec failed\n");
-      exit(1);
-    }
-    else {
-      // 父进程
-      wait(0);
+    else
+    {
+      arg[i++] = buf;
     }
   }
-
   return 0;
 }
