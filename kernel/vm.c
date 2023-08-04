2db4ae8ea2d28b6bc33d755aa05a641eaec1ca4f
MC J <jmc8@qq.com>
2023-08-04 11:38:57 +0800

    实现vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,36 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint_dfs(pagetable_t pagetable, uint depth)
+{
+  static char* prefix[] = {
+    [1] = "..",
+          ".. ..",
+          ".. .. .."
+  };
+
+  if (depth > 3) {
+    panic("vmprint_dfs: depth > 3");
+    return;
+  }
+
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      pte_t child = PTE2PA(pte);
+      printf("%s%d: pte %p pa %p\n", prefix[depth], i, pte, child);
+      if (child & (PTE_R | PTE_W | PTE_X) == 0) {
+        vmprint_dfs((pagetable_t)child, depth + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_dfs(pagetable, 1);
+})
