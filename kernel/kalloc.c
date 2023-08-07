d88f3cdbf4a2eb47da07f6b821d37189c9142abd
MC J <jmc8@qq.com>
2023-08-07 17:07:45 +0800

    完善alloc与free

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -68,6 +68,12 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  if (cow_cnt[PA2IDX(r)] > 1) // 只有引用计数为1时才释放
+  { 
+    dec_ref(r);
+    return;
+  }
+
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -99,6 +105,9 @@ kalloc(void)
   release(&kmem.lock);
 
   if(r)
+  {
+    cow_cnt[PA2IDX(r)] = 1;      // 将引用计数置1
     memset((char*)r, 5, PGSIZE); // fill with junk
+  }
   return (void*)r;
 }
